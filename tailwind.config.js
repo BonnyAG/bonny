68f2a819a1ca14a8d765fbd87bffde1a60514c1f
Adam Bonny <agbonnyh@gmail.com>
2022-06-03 14:33:26 -0600

    Added links to homepage & updated design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'body': ['Open Sans', 'sans-serif'],
+      'title': ['Rubik Mono One', 'sans-serif'],
+      'subtitle': ['Roboto Mono', 'monospace'],
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
